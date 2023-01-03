58bd3105db80402f1beedffa278c022f7acbfadd
voidgxd <118388964+voidgxd@users.noreply.github.com>
2023-01-03 15:41:55 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Максим Мосалёв on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
